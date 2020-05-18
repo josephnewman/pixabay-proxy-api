6a2c1599119a58f0771555e85d2dbe2d3afb937b
Joe Newman <joe.newman@outlook.com>
2020-05-18 13:23:27 +0100

    Resolve cors issues

--- index.js
+++ index.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const cors = require('cors');
+
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const app = express();
+app.use(cors());
+app.options('*', cors());
 
 const PORT = process.env.PORT || 3000;
 
@@ -10,9 +14,10 @@ console.log(`Listening on port ${PORT}`);
 app.use(
   '/api',
   createProxyMiddleware({
-    target: `https://pixabay.com/api/?key=${process.env.API_KEY}`,
+    target: `https://pixabay.com/api/?key=${process.env.API_KEY}&`,
     changeOrigin: true,
-    logLevel: 'warn',
+    logLevel: 'debug',
   })
 );
+
 app.listen(PORT);
